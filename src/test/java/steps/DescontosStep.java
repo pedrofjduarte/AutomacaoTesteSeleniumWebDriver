5fa6fa02c7bbefd36f505a281aa7f027da555a56
Pedro Junio <pedrofjduarte@gmail.com>
2021-10-02 23:37:34 -0300

    Implementação da funcionalidade 'Receber desconto da qazando' com BDD utilizando Cucumber. Automatização com Selenium fazendo o acesso a página da QAZando.

---
+++ src/test/java/steps/DescontosStep.java
@@ -0,0 +1,43 @@
+package steps;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DescontosStep {
+
+    WebDriver driver = new ChromeDriver();
+
+    @Dado("^que estou no site da qazando$")
+    public void acessar_site_qazando(){
+        driver.get("http://qazando.com.br/curso.html");
+    }
+
+    @Quando("^eu preencho meu e-mail$")
+    public void eu_preencho_meu_e_mail() throws InterruptedException {
+
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("window.scrollTo(0, 10000");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("email")).sendKeys("pedroj@qazando.com.br");
+    }
+
+    @Quando("^clico em ganhar cupom$")
+    public void clico_em_ganhar_cupom(){
+        driver.findElement(By.id("button")).click();
+    }
+
+    @Então("^eu vejo o código de desconto$")
+    public void eu_vejo_o_código_de_desconto() {
+        String textoCupom = driver.findElement(By.cssSelector("#cupom > h2 > span")).getText();
+        System.out.println(textoCupom);
+
+        driver.quit();
+    }
+
+}
